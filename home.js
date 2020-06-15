cc9b523065b5224fd24451cb345b546d92700bb0
ludobar99 <ludobar99@gmail.com>
2020-06-15 12:06:11 +0200

    two distinct forms for sitter and pet

--- home.js
+++ home.js
@@ -1,151 +1,64 @@
 
-$( document ).ready(function() {       
-                    //PET FILTERS
-                    //PET cat/dog
-                    $('<div/>', {
-                        'id':'pet-row',
-                        'class':'row'
-                    }).appendTo('#filter');
+$(document).ready(function() { 
 
-                    $('<div/>', {
-                        'id':'first-pet',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#pet-row');
-                    $('<label/>', {
-                        'text': 'Dog',
-                        'for': 'dog',
-                        'style': 'padding: 0 15px 0 0 !important'
-                    }).appendTo('#first-pet');
-                    $('<input/>', {
-                        'type': 'checkbox',
-                        'name': 'pet',
-                        'value': 'dog'
-                    }).appendTo('#first-pet');
-                    $('<div/>', {
-                        'id':'second-pet',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#pet-row');
-                    $('<label/>', {
-                        'text': 'Cat',
-                        'for': 'cat',
-                        'style': 'padding: 0 15px 0 0 !important'
-                    }).appendTo('#second-pet');
-                    $('<input/>', {
-                        'type': 'checkbox',
-                        'name': 'pet',
-                        'value': 'cat'
-                    }).appendTo('#second-pet');
-                    //SIZE
-                    $('<div/>', {
-                        'id':'size-row',
-                        'class':'row'
-                    }).appendTo('#filter');
+    $('#petsitterFilter').hide();
+    $('#petFilter').hide();
+    $("#owner-sitter").change(function (){ 
 
-                    $('<div/>', {
-                        'id':'first',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#size-row');
-                    $('<label/>', {
-                        'text': 'big',
-                        'for': 'big',
-                        'style': 'padding: 0 15px 0 0 !important'
-                    }).appendTo('#first');
-                    $('<input/>', {
-                        'type': 'checkbox',
-                        'name': 'size',
-                        'value': 'big'
-                    }).appendTo('#first');
-                    
-                    $('<div/>', {
-                        'id':'second',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#size-row');
-                    $('<div/>', {
-                        'id':'second',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#filter');
-                    $('<label/>', {
-                        'text': 'medium',
-                       'for': 'medium',
-                        'style': 'padding: 0 15px 0 0 !important'
-                    }).appendTo('#second');
-                    $('<input/>', {
-                        'type': 'checkbox',
-                        'name': 'size',
-                        'value': 'medium'
-                    }).appendTo('#second');
+    if (document.getElementById('owner-sitter').value == "Pet") {
+    //pet features: pet, size, calendar
+        $('#toHide').hide();
+        $('#petsitterFilter').hide();
+        $('#petFilter').show();
+    } else {
+        $('#toHide').hide();
+        $('#petsitterFilter').show();
+        $('#petFilter').hide();
+    }         
+    });
 
-                    $('<div/>', {
-                        'id':'third',
-                        'class':'form-check form-check-group'
-                    }).appendTo('#size-row');
-                    $('<label/>', {
-                        'text': 'small',
-                        'for': 'small',
-                        'style': 'padding: 0 15px 0 0 !important'
-                    }).appendTo('#third');
-                    $('<input/>', {
-                        'type': 'checkbox',
-                        'name': 'size',
-                        'value': 'small'
-                    }).appendTo('#third');
-                    $('#pet-row').hide();
-                    $('#size-row').hide();
-                    // PET SITTER FILTERS - max price, calendar
-                    $('<label/>', {
-                        'id': 'priceSlide-label',
-                        'for':'#priceSlide',
-                        'text': 'Max price '
-                    }).appendTo('#filter');
-                    $('<input/>', {
-                        'id':'priceSlide',
-                        'class':'no-border',
-                        'type':'range',
-                        'value':'0',
-                        'min':'0',
-                        'max':'30'
-                    }).appendTo('#filter');
-                    $('<button/>', {
-                        'id':'sub',
-                        'class':'btn btn-primary',
-                        'type':'submit',
-                        'text': 'submit'
-                    }).appendTo('#filter');
+    $('#petForm').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: 'petfilter.php',
+            data: $(this).serialize(),
+            success: function(response)
+            {
+                var jsonData = JSON.parse(response);
+                if (jsonData.success == "1")
+                {
+                    location.href = 'my_profile.php';
+                }
+                else
+                {
+                    alert('Invalid Credentials!');
+                }
+            }
+        });
+    });
 
-                    $('#priceSlide').hide();
-                    $('#priceSlide-label').hide();
-                    $('#sub').hide();
-                    $("#owner-sitter").change(function (){ 
+    $('#petsitterForm').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: 'petfilter.php',
+        data: $(this).serialize(),
+        success: function(response)
+        {
+            var jsonData = JSON.parse(response);
 
-                    if (document.getElementById('owner-sitter').value == "Pet") {
-                    //pet features: pet, size, calendar
-                    $('#toHide').hide();
-                    $('#priceSlide').hide();
-                    $('#priceSlide-label').hide();
-                    $('#pet-row').show();
-                    $('#size-row').show();
-                    $('#sub').show();
-                    
-                    } else {
-                        $('#toHide').hide();
-                        $('#pet-row').hide();
-                        $('#size-row').hide();
-                        $('#priceSlide').show();
-                        $('#priceSlide-label').show();
-                        $('#sub').show();
-                    }         
-                });
-
-                $('form').on('submit', function(e) {
-                    e.preventDefault();
-                    $.ajax({
-                      type: 'post',
-                      url: 'filter.php',
-                      data: $('form').serialize(),
-                      success: function () {
-                        alert('form was submitted');
-                      }
-                    });
-          
-                  });
-            });     
+            // user is logged in successfully in the back-end
+            // let's redirect
+            if (jsonData.success == "1")
+            {
+                location.href = 'my_profile.php';
+            }
+            else
+            {
+                alert('Invalid Credentials!');
+            }
+        }
+    });
+});
+});     
